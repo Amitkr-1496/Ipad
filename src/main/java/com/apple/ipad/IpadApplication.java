c9a935883fc04baee71904a8642aee217fcda163
Amitkr-1496 <amitkr9614@gmail.com>
2024-08-07 13:42:00 +0530

    Initial Project Build

---
+++ src/main/java/com/apple/ipad/IpadApplication.java
@@ -0,0 +1,13 @@
+package com.apple.ipad;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IpadApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IpadApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/apple/ipad/IpadApplicationTests.java
@@ -0,0 +1,13 @@
+package com.apple.ipad;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IpadApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
